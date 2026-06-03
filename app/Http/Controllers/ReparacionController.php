ef918019e6a9dfcd2a87eb4baebc6435b3c0d24f
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-06-03 19:56:14 -0300

    controller

--- app/Http/Controllers/ReparacionController.php
+++ app/Http/Controllers/ReparacionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reparacion;
 use Illuminate\Http\Request;
 
 class ReparacionController extends Controller
@@ -11,7 +12,11 @@ class ReparacionController extends Controller
      */
     public function index()
     {
-        //
+        
+    $reparaciones = Reparacion::all();
+
+    return view('reparaciones.index', compact('reparaciones'));
+
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReparacionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('reparaciones', ReparacionController::class);
