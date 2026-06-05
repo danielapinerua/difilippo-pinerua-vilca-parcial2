a6aabe32ffe6e0fdf11ee50ad0273e5a173abde6
Marcos Di Filippo <marcosjesusdifilippo@gmail.com>
2026-06-05 18:12:23 -0300

    Cambiamos configuracion en auth.php para que apunte a nuestro model de Usuario
    Cambiamos configuracion en auth.php para que apunte a nuestro model de Usuario

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
             'password' => ['required'],
         ]);
         
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials)) {            
             $request->session()->regenerate();
             return redirect()->route('reparaciones.index');
         }
