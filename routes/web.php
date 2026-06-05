ed19e0d118e9d29d8748fda1b1f4ed8fef99517d
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-06-04 21:16:35 -0300

    actualice web php

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ReparacionController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\CelularController;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-use App\Http\Controllers\ReparacionController;
 Route::resource('reparaciones', ReparacionController::class);
-
-use App\Http\Controllers\MarcaController;
 Route::resource('marcas', MarcaController::class);
-
-use App\Http\Controllers\CelularController;
 Route::resource('celulares', CelularController::class);
+Route::resource('clientes', ClienteController::class);
