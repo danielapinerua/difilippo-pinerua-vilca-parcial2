faaf98e730ca99be4e1427bad9d914c69aa57871
danielapinerua <daniela.pinerua@davinci.edu.ar>
2026-06-06 23:57:00 -0300

    descripcion

--- resources/views/reparaciones/index.blade.php
+++ resources/views/reparaciones/index.blade.php
@@ -20,31 +20,31 @@
         <th>Marca</th>
         <th>Estado</th>
         <th>Técnico</th>
+        <th>Descripción</th>
         <th>Acciones</th>
     </tr>
 
     @foreach($reparaciones as $reparacion)
-        <tr>
-            <td>{{ $reparacion->id }}</td>
-            <td>{{ $reparacion->cliente->nombre }}</td>
-            <td>{{ $reparacion->celular->modelo }}</td>
-            <td>{{ $reparacion->celular->marca->nombre }}</td>
-            <td>{{ $reparacion->estado }}</td>
-            <td>{{ $reparacion->usuario->name }}</td>
+    <tr>
+    <td>{{ $reparacion->id }}</td>
+    <td>{{ $reparacion->cliente->nombre }}</td>
+    <td>{{ $reparacion->celular->modelo }}</td>
+    <td>{{ $reparacion->celular->marca->nombre }}</td>
+    <td>{{ $reparacion->estado }}</td> 
+    <td>{{ $reparacion->usuario->name }}</td> 
+    <td>{{ $reparacion->descripcion_falla }}</td>
 
-            <td>
-                <a href="{{ route('reparaciones.show', $reparacion->id) }}">Ver</a>
+    <td>
+        <a href="{{ route('reparaciones.show', $reparacion->id) }}">Ver</a>
+        <a href="{{ route('reparaciones.edit', $reparacion->id) }}">Editar</a>
 
-                <a href="{{ route('reparaciones.edit', $reparacion->id) }}">Editar</a>
-
-
-                <form action="{{ route('reparaciones.destroy', $reparacion->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Eliminar</button>
-                </form>
-            </td>
-        </tr>
+        <form action="{{ route('reparaciones.destroy', $reparacion->id) }}" method="POST" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Eliminar</button>
+        </form>
+    </td>
+</tr>
     @endforeach
 
 </table>
